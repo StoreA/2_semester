0f830b691397260c9e5905d255363b922c388c11
StoreA <58264774+StoreA@users.noreply.github.com>
2025-02-27 09:18:28 +0100

    Test environment setup

---
+++ main/test.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN  
+#include "../include/catch.hpp"
+
+int add(int a, int b) {
+    return a + b;  // Dummy funktion test
+}
+
+TEST_CASE("Addition fungerer korrekt", "[math]") {
+    REQUIRE(add(2, 2) == 4);
+    REQUIRE(add(-1, 1) == 0);
+    REQUIRE(add(10, 5) == 15);
+}
